6be5e1b4426bfdfe00d2f394af5cf43f2357addf
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-04-23 15:29:22 +0100

    make test webpage

---
+++ app.mjs
@@ -0,0 +1,18 @@
+//Importing frameworks
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.render("index.html")
+});
+
+app.get("/api/health", (req, res) => {
+ res.json({ ok: true }); });
+
+app.listen(port, () => {
+   console.log(`Server running on port ${port}`);
+ });
